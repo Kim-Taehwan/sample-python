550367c7414801cde430f7478d744ce2285b6f31
dave <thkim02@trenbe.com>
2022-04-27 11:26:15 +0900

    init

---
+++ sample/practice.py
@@ -0,0 +1,58 @@
+print(51111)
+print(-1111)
+print(3.144)
+print(100000000)
+print(5+4)
+print(5*3)
+print(3*(3+1))
+
+print('풍선')
+print('나비')
+print('ㅋ'*19)
+
+# 참 / 거짓
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+
+print(not (5 > 10))
+
+# 애완동물을 소개해 주세요
+animal = '강아지'
+name = '연탄이'
+age = 4
+hobby = '산책'
+is_adult = age >= 3
+
+print('우리집 ' + animal + '의 이름은 ' + name + '에요')
+print('연탄이는 ' + str(age) + '살이며, ' + hobby + '을 아주 좋아해요')
+print('연탄이는 어른일까요? ' + str(is_adult))
+
+print('우리집', animal, '의 이름은 ' + name + '에요')
+
+'''1
+주
+석
+입
+니
+# 다
+# '''
+
+station = '사당'
+print(station, '행 열차가 들어오고 있습니다.')
+station = '신도림'
+print(station, '행 열차가 들어오고 있습니다.')
+station = '인천공항'
+print(station, '행 열차가 들어오고 있습니다.')
+
+print(2*3)
+print(2**3)
+print(10//3)
+
+print(1 != 3)
+print(not 1 == 3)
+
+print(3 > 0 and 3 > 5)
