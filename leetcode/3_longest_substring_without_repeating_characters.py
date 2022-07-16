11cb7173d546dceabd004180e958e27cbacccd62
taehwankim <wih0202@gmail.com>
2022-07-16 23:41:00 +0900

    first

---
+++ leetcode/3_longest_substring_without_repeating_characters.py
@@ -0,0 +1,12 @@
+import logging
+
+
+class Solution:
+    def length_of_longest_substring(self, s: str) -> int:
+        return 0
+
+
+_s = 'abcabcbb'
+sol = Solution()
+answer = sol.length_of_longest_substring(_s)
+logging.info(f'test:{answer}')
