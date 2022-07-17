0ebfb48e30167250abddc86ee48127a11b65ca54
taehwankim <wih0202@gmail.com>
2022-07-17 21:13:45 +0900

    v2

--- leetcode/3_longest_substring_without_repeating_characters.py
+++ leetcode/3_longest_substring_without_repeating_characters.py
@@ -1,12 +1,47 @@
 import logging
+import string
+
+
+def reset_alphabets(s):
+    # key-value 쌍 만들기
+    alphabets = {}
+    for c in s:
+        alphabets[c] = 0
+
+    return alphabets
 
 
 class Solution:
     def length_of_longest_substring(self, s: str) -> int:
-        return 0
+        alphabets = reset_alphabets(s)
+        max_cnt = 0
+        tmp = 0
+
+        if len(s) == 0:
+            return 0
+
+        if len(s.strip()) == 0 or len(s) == 1:
+            return 1
+
+        for i in s:
+            value = alphabets.get(i)
+            if value == 0:
+                alphabets[i] = 1
+                tmp = tmp + 1
+            else:
+                max_cnt = max(tmp, max_cnt)
+                tmp = 0
+                alphabets = reset_alphabets(s)
+
+                alphabets[i] = 1
+                tmp = tmp + 1
+                max_cnt = max_cnt - 1
+
+        return max(tmp, max_cnt)
 
 
-_s = 'abcabcbb'
 sol = Solution()
-answer = sol.length_of_longest_substring(_s)
-print(f'test:{answer}')
+_s = ['abcabcbb', 'a cab2c#bb', 'b bbb', 'pwwkew', " ", "c", "au", "dvdf"]
+for ss in _s:
+    answer = sol.length_of_longest_substring(ss)
+    print(f'test:{answer}')
