a115af98a925b30bf5a006e892d5d1bf39a3b4f5
taehwankim <wih0202@gmail.com>
2022-07-19 23:52:55 +0900

    solution 2

--- leetcode/3_longest_substring_without_repeating_characters.v3.py
+++ leetcode/3_longest_substring_without_repeating_characters.v3.py
@@ -22,49 +22,41 @@ import string
     5. 같은 문자가 나모면,
         a. start_index = start_index + 1 로 변경한다.
         b. max_cnt = max (max_cnt, length_cnt) 로 계산한다.
+        c. dict 를 초기화 한다.
     6. 다시 반복문을 실행하면서, 3, 4, 5 를 실행한다.
     7. 문자열의 끝에 도달하면, max_cnt 를 리턴한다.
 """
 
-class Solution:
-    def reset_alphabets(self, _str):
-        # key-value 쌍 만들기
-        alphabets = {}
-        for c in _str:
-            alphabets[c] = 0
 
-        return alphabets
+class Solution2:
 
     def length_of_longest_substring(self, s: str) -> int:
-        sol = Solution()
-        char_dict = {}
+        start_index = 0
+        length_cnt = 0
+        max_cnt = 0
+        to_array = list(s)
 
-        max_no = 0
-        tmp = 0
-        char_list = list(s)
-        print(f'char_list: {char_list}')
-        for c in s:
-            value = char_dict.get(c)
-            print(f'c: {c}, value: {value}')
+        for ch in s:
+            char_dict = {}
 
-            if value is None:
-                char_dict[c] = 1
-                tmp = tmp + 1
-            else:
-                print(f'tmp: {tmp}')
-                print(f'max_no: {max_no}')
-                max_no = max(tmp, max_no)
-                char_dict = {}
-                break
+            for c in to_array[start_index:]:
+                value = char_dict.get(c)
+                if value is None:
+                    char_dict[c] = 1
+                    length_cnt = length_cnt + 1
+                else:
+                    break
 
-        print(f'max_no: {max_no}')
-        print(char_dict)
-        return 0
+            max_cnt = max(max_cnt, length_cnt)
+            start_index = start_index + 1
+            length_cnt = 0
+
+        return max_cnt
 
 
-solution = Solution()
-args = ["dvdf"]
-# args = ['abcabcbb', 'a cab2c#bb', 'b bbb', 'pwwkew', " ", "c", "au", "dvdf"]
+solution = Solution2()
+# args = ["dvdf", "abcabcbb"]
+args = ['abcabcbb', 'a cab2c#bb', 'b bbb', 'pwwkew', " ", "c", "au", "dvdf"]
 
 for s in args:
     answer = solution.length_of_longest_substring(s)
