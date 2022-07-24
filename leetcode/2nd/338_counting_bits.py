533535ad9166eb9189ea4e2eee31055259f3cad6
taehwankim <wih0202@gmail.com>
2022-07-25 00:25:47 +0900

    dp 문제

--- leetcode/2nd/338_counting_bits.py
+++ leetcode/2nd/338_counting_bits.py
@@ -3,8 +3,18 @@ from typing import List
 
 class Solution:
     def count_bits(self, n: int) -> List[int]:
-        pass
+        result = []
+        for i in range(n + 1):
+            result.append(bin(i).count('1'))
+
+        return result
 
 
+num_list = [2, 5]
+solution = Solution()
+
+for _n in num_list:
+    print(f'num: {_n}')
+    print(solution.count_bits(_n))
 
 
